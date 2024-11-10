90873cc40b86f7022a6565ea043a18af7751eea9
kolay404 <angelnicole.b11@gmail.com>
2024-11-10 16:28:40 +0800

    added the hamburger

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import './Header.css'; // Adjust the path if necessary
 
 const Header = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
   const handleBackClick = () => {
     window.history.back(); // Go back to the previous page in history
+  };
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
   };
 
   return (
@@ -14,8 +22,18 @@ const Header = () => {
       <img src="/cit-logo2.png" alt="CITU Logo2" className="logo2-image" />
       <nav>
         <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/Pagbasa">PAGBASA AT PAGSULAT SA IBA’T IBANG DISCIPLINA</a></li>
+          <li className="hamburger-icon">
+            <button onClick={toggleMenu} className="hamburger-button">
+              &#9776; {/* Hamburger icon */}
+            </button>
+            {menuOpen && (
+              <ul className="dropdown-menu">
+                <li><Link to="/">Home</Link></li> {/* Updated to use Link */}
+                <li><Link to="/Sining">SINING NG PAKIKIPAGTALASTASAN</Link></li> {/* Updated to use Link */}
+                <li><Link to="/Pagbasa">PAGBASA AT PAGSULAT SA IBA’T IBANG DISCIPLINA</Link></li> {/* Updated to use Link */}
+              </ul>
+            )}
+          </li>
         </ul>
       </nav>
     </header>
